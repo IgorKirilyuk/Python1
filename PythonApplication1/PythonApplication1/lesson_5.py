9c7998fa748603f2b5923b989b860b94f9672f64
gokir@yandex.ru <101Pflfybt>
2023-07-10 09:29:11 +0300

    Update lesson_5.py

--- PythonApplication1/PythonApplication1/lesson_5.py
+++ PythonApplication1/PythonApplication1/lesson_5.py
@@ -14,9 +14,9 @@ else: print('nechetnoe chislo')
 ##Task 5_2
 i = str(input())
 ni_ = len(i)
-g_ = 'eyioau'
+g_ = 'eioau'
 ng_ = len(g_)
-s_ = 'qwrtpsdfghjklzxcvbnm'
+s_ = 'yqwrtpsdfghjklzxcvbnm'
 ns_ = len(s_)
 gs_ = 0
 ss_ = 0
